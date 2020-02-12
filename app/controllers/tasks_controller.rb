81dd8fb211e11d41bb75952dd94bf93d9883fabf
shironeko <shiro55222@gmail.com>
2020-02-12 06:43:21 +0000

    add status-column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -46,6 +46,6 @@ class TasksController < ApplicationController
   
   #strong parameter
   def task_params
-    params.require(:task).permit(:content, :title)
+    params.require(:task).permit(:content, :title, :status)
   end
 end
