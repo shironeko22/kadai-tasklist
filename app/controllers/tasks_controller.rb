272c6647f9547534fcaed60928bd46b0208041cb
shironeko <shiro55222@gmail.com>
2020-02-12 05:40:01 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -46,6 +46,6 @@ class TasksController < ApplicationController
   
   #strong parameter
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :title)
   end
 end
