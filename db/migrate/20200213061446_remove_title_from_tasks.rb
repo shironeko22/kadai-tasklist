a01ea7bf3e4902e1b2ecfe9a4927c1986a701bfa
shironeko <shiro55222@gmail.com>
2020-02-13 11:01:19 +0000

    delete migration-file

--- db/migrate/20200213061446_remove_title_from_tasks.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTitleFromTasks < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :tasks, :title, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_13_061446) do
+ActiveRecord::Schema.define(version: 2020_02_12_062419) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
