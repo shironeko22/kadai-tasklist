e8d5468646226629d76c8af3d5aa380d6c717ad1
shironeko <shiro55222@gmail.com>
2020-02-24 04:02:51 +0000

    before 1on1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get 'signup', to: 'users#new'
+  post "users", to: "users#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'tasks#index'
+  root to: 'toppages#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   resources :tasks
 end
