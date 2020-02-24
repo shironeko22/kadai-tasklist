09ec2685e995ca3b2128a54b8883760fe5b52408
shironeko <shiro55222@gmail.com>
2020-02-24 07:02:22 +0000

    add login-function & 1on1

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   post "users", to: "users#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'toppages#index'
+  root to: 'tasks#index'
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_24_024323) do
+ActiveRecord::Schema.define(version: 2020_02_24_043413) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2020_02_24_024323) do
     t.datetime "updated_at", null: false
     t.string "status"
     t.string "title"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -28,4 +30,5 @@ ActiveRecord::Schema.define(version: 2020_02_24_024323) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "tasks", "users"
 end
